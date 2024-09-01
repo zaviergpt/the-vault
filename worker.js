5dc3bd1825f9040246c1123c3d0b3b6ebafbf8cb
Zavier Chiam <60832719+zaviergpt@users.noreply.github.com>
2024-09-01 18:49:16 +0800

    Update worker.js

---
+++ worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'the-vault';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/icon192.png',
+  '/icon512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
